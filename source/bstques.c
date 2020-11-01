5aa4a1dc071d694af83cdddcc267d2d658184c34
Sunny Kumar <sunny.kumar@e-arc.in>
2020-11-01 12:51:07 +0530

    bstquestions

---
+++ source/bstques.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(){
+    int a[] = { 1,2,4,5,11,34,5,23,87,23,45};
+    int w = 3;
+    //findMaxInSlidingwindow(a);
+    return 0;
+}
